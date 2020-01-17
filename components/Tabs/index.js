3bb0a24818ae65d45de5a12a38324879b8483cf7
Josiah <reikiryo@yahoo.com>
2020-01-17 11:22:11 -0500

    topics component functional

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+function newTab(str){
+    //creating component peices
+    const tab = document.createElement('div')
+    //adding classes
+    tab.classList.add('tab')
+    //text content 
+    tab.textContent = str
+
+    return tab
+}
+
+const topics = document.querySelector('.topics')
+//getting server response and adding each response to a new tab
+//each new tab is appended to the topics section in index.html
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+.then(res => {
+    res.data.topics.forEach(topic => {
+        topics.append(newTab(topic))
+    })
+})
+.catch(err => {
+    console.log(err)
+})
